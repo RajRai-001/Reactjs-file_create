ddce2cf068ceacde415d894adbef6f54c036499d
Raj <rai8604398971@gmail.com>
2026-06-24 21:45:02 +0530

    Updated files

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+     <h1 className='bg-amber-600'>app</h1>
+    </div>
+  )
+}
+
+export default App
